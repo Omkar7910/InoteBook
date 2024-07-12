f78c6d5d3d06b9a0da15123b4767288a6ab3f452
Omkar7910 <omkarpatil7910@gmail.com>
2024-07-12 13:16:15 +0530

    commit1

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/inotebook"
+
+const connectToMongo = () => {
+  mongoose.connect(mongoURI)
+    .then(() => {
+      console.log("Connected to MongoDB Successfully");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+    });
+};
+
+module.exports = connectToMongo;
